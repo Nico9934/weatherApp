683ab9c72bb0387b5e58aee65ff5443f02a9019c
Nico9934 <nico9934@gmail.com>
2023-09-01 10:27:57 -0300

    Styles day

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,96 +1,114 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Spinner from "./Spinner";
 import Forecast from "./Forecast";
 
-const Card = ({ show, loading, weather, backgroundImage, forecast }) => {
-  const [time, setTime] = useState("");
+const Card = ({show, loading, weather, backgroundImage, forecast}) => {
+    const [time, setTime] = useState("");
 
-  const day = new Date().getDate();
-  const month = new Date().getMonth() + 1;
-  const year = new Date().getFullYear();
-  const hour = new Date().getHours();
-  const minutes = new Date().getMinutes();
+    const day = new Date().getDate();
+    const month = new Date().getMonth() + 1;
+    const year = new Date().getFullYear();
+    const hour = new Date().getHours();
+    const minutes = new Date().getMinutes();
+    let actualDay;
 
-  const actualDay = `${day}/${month}/${year}`;
-  const actualHour = `${hour}:${minutes < 10 ? "0" + minutes : minutes}`;
-  if (loading) {
-    return <Spinner />;
-  }
+    if (day < 10) {
+        actualDay = `0${day}/${month}/${year}`;
+    } else {
+        actualDay = `${day}/${month}/${year}`;
+    }
 
-  return (
-    <div>
-      {show ? (
-        <div className="container-app m-auto w-full md:w-9/12 rounded-md mt-10 mb-10">
-          <div className="h-auto gap-5 md:grid sm:flex grid-cols-5 ">
-            <div className="col-start-1 col-end-3">
-              <img
-                className="h-full object-cover"
-                src={backgroundImage}
-                alt=""
-              />
-            </div>
-            <div className="p-5 col-start-3 col-end-6">
-              {/* Datos ciudad */}
-              <div>
-                <h1 className="text-3xl w-full mb-1 first-letter:uppercase font-bold text-gray-500 text-center">
-                  {weather.name}
-                </h1>
+    const actualHour = `${hour}:${minutes < 10 ? "0" + minutes : minutes}`;
+    if (loading) {
+        return <Spinner />;
+    }
 
-                <div className="flex mb-5">
-                  <div className="flex flex-col w-full justify-evenly sm:column">
-                    <p className="text-sm uppercase font-bold  text-gray-500">
-                      {actualDay}
-                    </p>
-                    <p className="text-sm uppercase font-bold  text-gray-500">
-                      {actualHour}
-                    </p>
-                    <p className="text-sm first-letter:uppercase  italic font-black  text-gray-600">
-                      {show && weather.weather[0].description}
-                    </p>
-                  </div>
+    return (
+        <div>
+            {show ? (
+                <div className="container-app m-auto w-full md:w-9/12 rounded-md mt-10 mb-10">
+                    <div className="h-auto gap-5 md:grid sm:flex grid-cols-5 ">
+                        <div className="col-start-1 col-end-3">
+                            <img
+                                className="h-full object-cover"
+                                src={backgroundImage}
+                                alt=""
+                            />
+                        </div>
+                        <div className="p-5 col-start-3 col-end-6">
+                            {/* Datos ciudad */}
+                            <div>
+                                <h1 className="text-3xl w-full mb-1 first-letter:uppercase font-bold text-gray-500 text-center">
+                                    {weather.name}
+                                </h1>
 
-                  <img
-                    className="p-2"
-                    src={
-                      show &&
-                      `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
-                    }
-                    alt="Icono del clima"
-                  />
+                                <div className="flex mb-5">
+                                    <div className="flex flex-col w-full justify-evenly sm:column">
+                                        <p className="text-sm uppercase font-bold  text-gray-500">
+                                            {actualDay}
+                                        </p>
+                                        <p className="text-sm uppercase font-bold  text-gray-500">
+                                            {actualHour}
+                                        </p>
+                                        <p className="text-sm first-letter:uppercase  italic font-black  text-gray-600">
+                                            {show &&
+                                                weather.weather[0].description}
+                                        </p>
+                                    </div>
+
+                                    <img
+                                        className="p-2"
+                                        src={
+                                            show &&
+                                            `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+                                        }
+                                        alt="Icono del clima"
+                                    />
+                                </div>
+                            </div>
+                            {/* Datos Temperatura */}
+                            <div className="">
+                                <p className="uppercase font-semibold  md:mb-2 text-gray-500">
+                                    Temperatura:{" "}
+                                    <span className="font-bold text-xl">
+                                        {(weather.main.temp - 273.15).toFixed(
+                                            1
+                                        )}
+                                        °
+                                    </span>{" "}
+                                </p>
+                                <p className="uppercase font-bold  md:mb-2 text-gray-500">
+                                    Max:{" "}
+                                    <span>
+                                        {(
+                                            weather.main.temp_max - 273.15
+                                        ).toFixed(1)}
+                                        °
+                                    </span>
+                                </p>
+                                <p className="uppercase font-bold  md:mb-2 text-gray-500">
+                                    Min:{" "}
+                                    <span>
+                                        {(
+                                            weather.main.temp_min - 273.15
+                                        ).toFixed(1)}
+                                        °
+                                    </span>
+                                </p>
+                            </div>
+
+                            <Forecast forecast={forecast} />
+                        </div>
+                    </div>
                 </div>
-              </div>
-              {/* Datos Temperatura */}
-              <div className="">
-                <p className="uppercase font-semibold  md:mb-2 text-gray-500">
-                  Temperatura:{" "}
-                  <span className="font-bold text-xl">
-                    {(weather.main.temp - 273.15).toFixed(1)}°
-                  </span>{" "}
+            ) : (
+                <p className="md:mt-10 mt-5 text-center uppercase m-auto w-9/12 bg-slate-100 py-1 px-3 rounded-sm">
+                    {" "}
+                    No ingresaste ningún dato aún
                 </p>
-                <p className="uppercase font-bold  md:mb-2 text-gray-500">
-                  Max:{" "}
-                  <span>{(weather.main.temp_max - 273.15).toFixed(1)}°</span>
-                </p>
-                <p className="uppercase font-bold  md:mb-2 text-gray-500">
-                  Min:{" "}
-                  <span>{(weather.main.temp_min - 273.15).toFixed(1)}°</span>
-                </p>
-              </div>
-
-              <Forecast 
-              forecast={forecast}
-              />
-            </div>
-          </div>
+            )}
         </div>
-      ) : (
-        <p className="md:mt-10 mt-5 text-center uppercase m-auto w-9/12 bg-slate-100 py-1 px-3 rounded-sm">
-          {" "}
-          No ingresaste ningún dato aún
-        </p>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Card;
--- src/components/WeatherPanel.jsx
+++ src/components/WeatherPanel.jsx
@@ -1,94 +1,90 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Form from "./Form";
 import Card from "./Card";
 import Message from "./Message";
 import Forecast from "./Forecast";
 
 const WeatherPanel = ({setBackgroundApp}) => {
-  // Clima actual
-  const [weather, setWeather] = useState([]);
-  // Pronostico para proximas horas
-  const [forecast, setForecast] = useState([]);
-  // Spinner de carga mientras llega la respuesta de la API
-  const [loading, setLoading] = useState(false);
-  // Tarjeta visible o no, con los datos del Clima
-  const [show, setShow] = useState(false);
-  //Variable para validad si la ciudad existe o no
-  const [cityOk, setCityOk] = useState(true);
-  //Variable para imagen de fondo
-  const [backgroundImage, setBackgroundImage] = useState("")
+    // Clima actual
+    const [weather, setWeather] = useState([]);
+    // Pronostico para proximas horas
+    const [forecast, setForecast] = useState([]);
+    // Spinner de carga mientras llega la respuesta de la API
+    const [loading, setLoading] = useState(false);
+    // Tarjeta visible o no, con los datos del Clima
+    const [show, setShow] = useState(false);
+    //Variable para validad si la ciudad existe o no
+    const [cityOk, setCityOk] = useState(true);
+    //Variable para imagen de fondo
+    const [backgroundImage, setBackgroundImage] = useState("");
 
+    const getLocation = async (city) => {
+        setLoading(true);
+        setShow(false);
 
-  const getLocation = async (city) => {
-    setLoading(true);
-    setShow(false);
-    
-    
-    const urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c1ae368108e49a561a3f9eb7fef6fac5&lang=es`;
-    await fetch(urlWeather)
-      .then((respuesta) => respuesta.json())
-      .then((resultado) => {
-        if (resultado.cod === 200) {
-          setCityOk(true);
-          setWeather(resultado);
-        } else {
-          setCityOk(false);
+        try {
+            const urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c1ae368108e49a561a3f9eb7fef6fac5&lang=es`;
+            await fetch(urlWeather)
+                .then((respuesta) => respuesta.json())
+                .then((resultado) => {
+                    if (resultado.cod === 200) {
+                        setCityOk(true);
+                        setWeather(resultado);
+                    } else {
+                        setCityOk(false);
+                    }
+                });
+
+            const urlPixaBay = `https://pixabay.com/api/?key=25603168-f2ed363edb05d08bb4418e66c&q=${city}&image_type=photo&category=travel&min_width=5000&min_heigth=3000`;
+            await fetch(urlPixaBay)
+                .then((respuesta) => respuesta.json())
+                .then((resultado) => {
+                    if (resultado.hits[0] === undefined) {
+                        setBackgroundImage(
+                            "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Cumulonimbo_SE_de_Caracas.jpg/600px-Cumulonimbo_SE_de_Caracas.jpg"
+                        );
+                    } else {
+                        setBackgroundImage(resultado.hits[2].largeImageURL);
+                        setBackgroundApp(resultado.hits[0].largeImageURL);
+                    }
+                });
+
+            // Forecast
+            const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c1ae368108e49a561a3f9eb7fef6fac5&lang=es`;
+            const respuestaForecast = await fetch(urlForecast);
+            const dataForecast = await respuestaForecast.json();
+
+            setForecast(dataForecast);
+            setShow(true);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
         }
-      });
-    
-    const urlPixaBay = `https://pixabay.com/api/?key=25603168-f2ed363edb05d08bb4418e66c&q=${city}&image_type=photo&category=travel&min_width=5000&min_heigth=3000`
-      await fetch(urlPixaBay)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
+    };
 
-          if (resultado.hits[0] === undefined) {
-            setBackgroundImage("https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Cumulonimbo_SE_de_Caracas.jpg/600px-Cumulonimbo_SE_de_Caracas.jpg")
-          } else {
-            setBackgroundImage(resultado.hits[2].largeImageURL)
-            setBackgroundApp(resultado.hits[0].largeImageURL)
-            }
-          })
+    return (
+        <div>
+            <Form getLocation={getLocation} show={show} />
 
-    // Forecast
-    const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c1ae368108e49a561a3f9eb7fef6fac5&lang=es`;
-    const respuestaForecast = await fetch(urlForecast);
-    const dataForecast = await respuestaForecast.json();
-
-   
-  setForecast(dataForecast);
-  setShow(true);
-  setLoading(false);
-  };
-
-  return (
-    <div>
-      <Form 
-        getLocation={getLocation}
-        show={show} 
-      />
-
-      {!cityOk ? (
-        <Message>No se ha encontrado la ciudad</Message>
-      ) : (
-        <>
-        <Card
-          loading={loading}
-          show={show}
-          weather={weather}
-          backgroundImage={backgroundImage}
-          forecast={forecast}
-          setForecast={setForecast}
-          />
-        
-        </>
-      )}
-    </div>
-  );
+            {!cityOk ? (
+                <Message>No se ha encontrado la ciudad</Message>
+            ) : (
+                <>
+                    <Card
+                        loading={loading}
+                        show={show}
+                        weather={weather}
+                        backgroundImage={backgroundImage}
+                        forecast={forecast}
+                        setForecast={setForecast}
+                    />
+                </>
+            )}
+        </div>
+    );
 };
 
 export default WeatherPanel;
-
-
 
 // const responseWeather = await fetch(urlWeather);
 // const dataWeather = await responseWeather.json();
